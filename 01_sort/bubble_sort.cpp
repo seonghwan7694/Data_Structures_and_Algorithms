9ce40f480a0c7e91b302d1355a6d40dca4d1027c
seonghwan <seonghwan7694@gmail.com>
2022-06-06 09:29:11 +0900

    first commit

---
+++ 01_sort/bubble_sort.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define INF 100 + 1
+
+void SWAP(int &a, int &b){
+  int tmp = a;
+  a = b;
+  b = tmp;
+}
+
+int main(){
+  // fast I/O
+  ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+
+  // input data
+
+  int arr_size = 1e2;
+  int arr_element[arr_size];
+  srand(time(NULL));
+  for(int i = 0; i < arr_size; i++){
+    arr_element[i] = rand()%INF; 
+  }
+
+  // bubble_sort
+  for(int i = 0; i < arr_size; i++){
+    for(int j = 0; j < arr_size; j++){
+      if(arr_element[i] < arr_element[j]){
+        SWAP(arr_element[i], arr_element[j]);
+      }
+    }
+  }
+  
+  // °á°ú
+  for(int i = 0; i < arr_size; i++){
+    cout << arr_element[i] << "\n";
+  }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Data_Structures_and_Algorithms
 자료구조와 알고리즘 관련한 내용을 기록하는 레포입니다.
+
